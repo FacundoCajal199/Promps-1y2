31a5f917fe9c80c528a373dcfd64cc24cb03e3f7
facundo cajal <facundocajal199@gmail.com>
2023-06-01 02:01:38 -0300

    funcionando

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Titulos from "./components/Titulos"
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container,Button } from "react-bootstrap";
+function App() {
+  
+
+  return (
+    <>
+    <Titulos TituloModificado ="MY FRIENDS"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,8 @@
+ const Titulos = (props)=>{
+    return(
+<section>
+    <h1>HELLO {props.TituloModificado } </h1>
+</section>
+    );
+ };
+ export default Titulos;
